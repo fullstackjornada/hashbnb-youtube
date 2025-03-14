2a554e179db5b827742b1072c87477de488010ea
Full Stack Jornada <fullstackjornada@gmail.com>
2025-03-14 18:06:54 -0300

    fim da aula 4

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ user }) => {
   return (
     <header className="shadow-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-8">
@@ -41,7 +41,7 @@ const Header = () => {
         </Link>
 
         <Link
-          to="/login"
+          to={user ? "/account" : "/login"}
           className="flex items-center gap-2 rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md"
         >
           <svg
@@ -70,7 +70,11 @@ const Header = () => {
             />
           </svg>
 
-          <p className="max-w-20 truncate sm:max-w-32">Diego XYZ</p>
+          {user ? (
+            <p className="max-w-20 truncate sm:max-w-32">{user.name}</p>
+          ) : (
+            <></>
+          )}
         </Link>
       </div>
     </header>
