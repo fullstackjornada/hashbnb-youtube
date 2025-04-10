4e7efb1c6aa894465d4698dad0e522e33d9d5f87
Diego <diego@hashtagtreinamentos.com>
2025-04-10 16:27:11 -0300

    fim da aula 13

--- front-end/src/pages/Place.jsx
+++ front-end/src/pages/Place.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { useUserContext } from "../contexts/UserContext";
 import Perk from "../components/Perk";
+import Booking from "../components/Booking";
 
 const Place = () => {
   const { id } = useParams();
@@ -12,13 +13,43 @@ const Place = () => {
   const [checkin, setCheckin] = useState("");
   const [checkout, setCheckout] = useState("");
   const [guests, setGuests] = useState("");
+  const [booking, setBooking] = useState(null);
+  const [redirect, setRedirect] = useState(false);
+
+  const numberOfDays = (date1, date2) => {
+    const date1GMT = date1 + "GMT-03:00";
+    const date2GMT = date2 + "GMT-03:00";
+
+    const dateCheckin = new Date(date1GMT);
+    const dateCheckout = new Date(date2GMT);
+
+    return (
+      (dateCheckout.getTime() - dateCheckin.getTime()) / (1000 * 60 * 60 * 24)
+    );
+  };
+
+  useEffect(() => {
+    if (place) {
+      const axiosGet = async () => {
+        const { data } = await axios.get("/bookings/owner");
+        setBooking(
+          data.filter((booking) => {
+            console.log(booking.place._id, place._id);
+
+            return booking.place._id === place._id;
+          })[0],
+        );
+      };
+
+      axiosGet();
+    }
+  }, [place]);
 
   useEffect(() => {
     if (id) {
       const axiosGet = async () => {
         const { data } = await axios.get(`/places/${id}`);
 
-        console.log(data);
         setPlace(data);
       };
 
@@ -32,15 +63,32 @@ const Place = () => {
       : document.body.classList.remove("overflow-hidden");
   }, [overlay]);
 
-  const handleBooking = (e) => {
+  const handleBooking = async (e) => {
     e.preventDefault();
 
     if (checkin && checkout && guests) {
-      console.log("Fez uma reserva!");
+      const nights = numberOfDays(checkin, checkout);
+
+      const objBooking = {
+        place: id,
+        user: user._id,
+        price: place.price,
+        total: place.price * nights,
+        checkin,
+        checkout,
+        guests,
+        nights,
+      };
+
+      const { data } = await axios.post("/bookings", objBooking);
+
+      setRedirect(true);
     } else {
       alert("Preencha todas as informações antes de fazer uma reserva!");
     }
   };
+
+  if (redirect) return <Navigate to="/account/bookings" />;
 
   if (!place) return <></>;
 
@@ -76,6 +124,9 @@ const Place = () => {
           </div>
         </div>
 
+        {/* Booking */}
+        {booking ? <Booking booking={booking} place={true} /> : ""}
+
         {/* Grade de Imagens */}
         <div className="relative grid aspect-square gap-4 overflow-hidden rounded-2xl sm:aspect-[3/2] sm:grid-cols-[2fr_1fr] sm:grid-rows-2">
           {place.photos
@@ -86,6 +137,7 @@ const Place = () => {
                 src={photo}
                 alt="Imagem da Acomodação"
                 onClick={() => setOverlay(true)}
+                key={photo}
               />
             ))}
 
@@ -113,7 +165,7 @@ const Place = () => {
         </div>
 
         {/* Colunas */}
-        <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className={`grid ${booking ? "" : "grid-cols-1 md:grid-cols-2"}`}>
           <div className="order-2 flex flex-col gap-5 p-6 md:order-none">
             <div className="flex flex-col gap-2">
               <p className="text-lg font-bold sm:text-2xl">Descrição</p>
@@ -137,7 +189,7 @@ const Place = () => {
 
               <div className="flex flex-col gap-1">
                 {place.perks.map((perk) => (
-                  <div className="flex items-center gap-2">
+                  <div key={perk} className="flex items-center gap-2">
                     <Perk perk={perk}></Perk>
                   </div>
                 ))}
@@ -145,62 +197,66 @@ const Place = () => {
             </div>
           </div>
 
-          <form className="order-1 flex flex-col gap-4 self-center justify-self-center rounded-2xl border border-gray-300 px-4 py-3 sm:px-8 sm:py-4 md:order-none">
-            <p className="text-center text-lg font-bold sm:text-2xl">
-              Preço: R$ {place.price} por noite
-            </p>
+          {booking ? (
+            ""
+          ) : (
+            <form className="order-1 flex flex-col gap-4 self-center justify-self-center rounded-2xl border border-gray-300 px-4 py-3 sm:px-8 sm:py-4 md:order-none">
+              <p className="text-center text-lg font-bold sm:text-2xl">
+                Preço: R$ {place.price} por noite
+              </p>
 
-            {/* Checkin e Checkout */}
-            <div className="flex flex-col sm:flex-row">
-              <div className="rounded-tl-2xl rounded-tr-2xl border border-gray-300 px-4 py-2 sm:rounded-tr-none sm:rounded-bl-2xl">
-                <p className="font-bold">Checkin</p>
+              {/* Checkin e Checkout */}
+              <div className="flex flex-col sm:flex-row">
+                <div className="rounded-tl-2xl rounded-tr-2xl border border-gray-300 px-4 py-2 sm:rounded-tr-none sm:rounded-bl-2xl">
+                  <p className="font-bold">Checkin</p>
+                  <input
+                    className="w-full sm:w-auto"
+                    type="date"
+                    value={checkin}
+                    onChange={(e) => setCheckin(e.target.value)}
+                  />
+                </div>
+
+                <div className="rounded-br-2xl rounded-bl-2xl border border-t-0 border-gray-300 px-4 py-2 sm:rounded-tr-2xl sm:rounded-bl-none sm:border-t sm:border-l-0">
+                  <p className="font-bold">Checkout</p>
+                  <input
+                    type="date"
+                    className="w-full sm:w-auto"
+                    value={checkout}
+                    onChange={(e) => setCheckout(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              {/* Convidados */}
+              <div className="flex flex-col gap-2 rounded-2xl border border-gray-300 px-4 py-2">
+                <p className="font-bold">N° de convidados</p>
                 <input
-                  className="w-full sm:w-auto"
-                  type="date"
-                  value={checkin}
-                  onChange={(e) => setCheckin(e.target.value)}
+                  type="number"
+                  className="rounded-2xl border border-gray-300 px-4 py-2"
+                  placeholder="2"
+                  value={guests}
+                  onChange={(e) => setGuests(e.target.value)}
                 />
               </div>
 
-              <div className="rounded-br-2xl rounded-bl-2xl border border-t-0 border-gray-300 px-4 py-2 sm:rounded-tr-2xl sm:rounded-bl-none sm:border-t sm:border-l-0">
-                <p className="font-bold">Checkout</p>
-                <input
-                  type="date"
-                  className="w-full sm:w-auto"
-                  value={checkout}
-                  onChange={(e) => setCheckout(e.target.value)}
-                />
-              </div>
-            </div>
-
-            {/* Convidados */}
-            <div className="flex flex-col gap-2 rounded-2xl border border-gray-300 px-4 py-2">
-              <p className="font-bold">N° de convidados</p>
-              <input
-                type="number"
-                className="rounded-2xl border border-gray-300 px-4 py-2"
-                placeholder="2"
-                value={guests}
-                onChange={(e) => setGuests(e.target.value)}
-              />
-            </div>
-
-            {user ? (
-              <button
-                className="bg-primary-400 w-full cursor-pointer rounded-full border border-gray-300 px-4 py-2 text-center font-bold text-white"
-                onClick={handleBooking}
-              >
-                Reservar
-              </button>
-            ) : (
-              <Link
-                to="/login"
-                className="bg-primary-400 w-full cursor-pointer rounded-full border border-gray-300 px-4 py-2 text-center font-bold text-white"
-              >
-                Faça seu login
-              </Link>
-            )}
-          </form>
+              {user ? (
+                <button
+                  className="bg-primary-400 w-full cursor-pointer rounded-full border border-gray-300 px-4 py-2 text-center font-bold text-white"
+                  onClick={handleBooking}
+                >
+                  Reservar
+                </button>
+              ) : (
+                <Link
+                  to="/login"
+                  className="bg-primary-400 w-full cursor-pointer rounded-full border border-gray-300 px-4 py-2 text-center font-bold text-white"
+                >
+                  Faça seu login
+                </Link>
+              )}
+            </form>
+          )}
         </div>
 
         {/* Extras */}
@@ -220,6 +276,7 @@ const Place = () => {
                   className={`aspect-square w-full object-cover`}
                   src={photo}
                   alt="Imagem da Acomodação"
+                  key={photo}
                 />
               ))}
             </div>
