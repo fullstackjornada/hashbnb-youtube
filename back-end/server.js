43adaf2ffee6744a17d4c14089d1a37cfbc0c5aa
Diego <diego@hashtagtreinamentos.com>
2025-04-10 17:02:36 -0300

    pre deploy

--- back-end/server.js
+++ back-end/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
 import { fileURLToPath } from "url";
-import { dirname } from "node:path";
+import path, { dirname } from "node:path";
 
 export const app = express();
 
@@ -19,4 +19,9 @@ app.use(
   })
 );
 app.use("/tmp", express.static(__dirname + "/tmp"));
-app.use(routes);
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+app.use("/api", routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
+});
